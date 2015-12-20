08f01d632d43b80a7782fbe16e585a484c49a0cb
Ma Yanlong <l.luffy.river@gmail.com>
2015-12-20 22:24:54 +0800

    use js2-mode

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,4 +1,4 @@
-
-(setq js-indent-level 2)
+(setq auto-mode-alist (cons `("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . js2-mode) auto-mode-alist))
+(setq-default js2-basic-offset 2)
 
 (provide 'init-javascript)
